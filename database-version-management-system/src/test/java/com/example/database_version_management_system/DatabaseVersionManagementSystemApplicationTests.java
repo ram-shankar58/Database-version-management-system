b82a1bfac09cd1a029a4c4623dbb7c5b6f3e09bf
ram-shankar58 <sanjayramshankar@gmail.com>
2024-05-26 00:41:12 +0530

    intial broiler done

---
+++ database-version-management-system/src/test/java/com/example/database_version_management_system/DatabaseVersionManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.database_version_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseVersionManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
